bf5e37043c67eba644725ad5c4ad5167039be0d9
Frank Herrmann <xpixer@googlemail.com>
2015-03-02 14:56:32 +0100

    - change chart size - remove extra chart style - add valuidx for important parameter

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,36 @@
 angular.module('starter.services', ['ngResource'])
 
 .factory('Sensor', function ($resource) {
-   // http://78.43.187.187:3080/topic/rollup/avg/1h?path=/sensors/411/power&start=-30000&prepare=true
-   return $resource('http://78.43.187.187:3080/topic/rollup/avg/1h', {}, {jsonpquery: { method: 'JSONP', params: {prepare: 'true', callback: 'JSON_CALLBACK'}, isArray: true}});
+   return $resource(
+      'http://78.43.187.187:3080/topic/rollup/avg/1h', 
+      {}, 
+      {
+         jsonpquery: { 
+            method: 'JSONP', 
+            params: {
+               prepare: 'true', 
+               callback: 'JSON_CALLBACK'
+            }, 
+            isArray: true
+         }
+      }
+   );
 })
 
 .factory('Session', function ($resource) {
-   return $resource('http://78.43.187.187:3081/geras', {}, {jsonpquery: { method: 'JSONP', params: {sub: 'groups', type: 'sensorgrp', callback: 'JSON_CALLBACK'}, isArray: true} });
+   return $resource(
+      'http://78.43.187.187:3081/geras', 
+      {}, 
+      {
+         jsonpquery: { 
+            method: 'JSONP', 
+            params: {
+               sub: 'groups', 
+               type: 'sensorgrp', 
+               callback: 'JSON_CALLBACK'
+            }, 
+            isArray: true
+         } 
+      }
+   );
 });
